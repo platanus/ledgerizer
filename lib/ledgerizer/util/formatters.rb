26cb4474f9622ef93cdf6ec6d2cbbcbb8b36d9b1
Leandro Segovia <ldlsegovia@gmail.com>
2019-08-24 18:49:43 -0300

    feat(formatters): check if model is actually an AR model

--- lib/ledgerizer/util/formatters.rb
+++ lib/ledgerizer/util/formatters.rb
@@ -1,9 +1,11 @@
 module Ledgerizer
   module Formatters
     def infer_active_record_class!(error_prefix, model_name)
-      model_name.to_s.classify.constantize
+      klass = model_name.to_s.classify.constantize
+      raise_non_ar_model_error(error_prefix) unless klass.ancestors.include?(ActiveRecord::Base)
+      klass
     rescue NameError
-      raise_formatter_error("#{error_prefix} must be an ActiveRecord model name")
+      raise_non_ar_model_error(error_prefix)
     end
 
     def format_currency!(currency)
@@ -12,6 +14,10 @@ module Ledgerizer
       return formatted_currency if Money::Currency.table.key?(formatted_currency)
 
       raise_formatter_error("invalid currency '#{currency}' given")
+    end
+
+    def raise_non_ar_model_error(error_prefix)
+      raise_formatter_error("#{error_prefix} must be an ActiveRecord model name")
     end
 
     def raise_formatter_error(msg)
